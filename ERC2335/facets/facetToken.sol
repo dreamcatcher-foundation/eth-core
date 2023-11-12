a6252df2f1f31e6e3d75a1b4676a5e40b4934e47
snqre <marco.bizxaro@gmail.com>
2023-11-12 17:23:36 +0000

    Update facetToken.sol

--- ERC2335/facets/facetToken.sol
+++ ERC2335/facets/facetToken.sol
@@ -64,6 +64,63 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
         token().maxSupply = newCap;
     }
 
+    function setTransferFee(uint newFee) public virtual {
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        /// Can be set from 0 to 1%. Will shout at you if its above 1%!
+        require(newFee <= 100, 'facetToken: fee is too high!');
+        token().feeTransfer = newFee;
+    }
+
+    function setTransferFeeDestination(address account) public virtual {
+        /// Set as zero to burn.
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        token().feeTransferTo = account;
+    }
+
+    function addToWhitelist(address account) public virtual {
+        if (token().enabledWhitelist) {
+            require(_msgSender() == admin().admin, 'facetToken: only admin');
+            token().whitelist.add(account);
+        } else {
+            revert('facetToken: whitelist disabled');
+        }
+    }
+
+    function removeFromWhitelist(address account) public virtual {
+        if (token().enabledWhitelist) {
+            require(_msgSender() == admin().admin, 'facetToken: only admin');
+            token().whitelist.remove(account);
+        } else {
+            revert('facetToken: whitelist disabled');
+        }
+    }
+
+    function addToBlacklist(address account) public virtual {
+        if (token().enabledBlacklist) {
+            require(_msgSender() == admin().admin, 'facetToken: only admin');
+            token().blacklist.add(account);
+        } else {
+            revert('facetToken: blacklist disabled');
+        }
+    }
+
+    function removeFromBlacklist(address account) public virtual {
+        if (token().enabledBlacklist) {
+            require(_msgSender() == admin().admin, 'facetToken: only admin');
+            token().blacklist.remove(account);
+        } else {
+            revert('facetToken: blacklist disabled');
+        }
+    }
+
+    function setWrappableToken(address newToken) public virtual {
+        if (token().enabledTokenWrapper) {
+            token().wrappableTokenIn = newToken;
+        } else {
+            revert('facetToken: token wrapper disabled');
+        }
+    }
+
     /// OPTIONAL To be set just after deployment.
     function enableCapped() public virtual {
         require(_msgSender() == admin().admin, 'facetToken: only admin');
@@ -79,6 +136,7 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     /// OPTIONAL To be set just after deployment.
     function enableWhitelist() public virtual {
         require(_msgSender() == admin().admin, 'facetToken: only admin');
+        addToWhitelist(address(0));
         token().enabledWhitelist = true;
     }
 
@@ -94,7 +152,35 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
         token().enabledTransferFee = true;
     }
 
-    /// TODO
+    /// OPTIONAL To be set just after deployment.
+    function enableSnapshot() public virtual {
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        token().enabledSnapshot = true;
+    }
+
+    /// OPTIONAL To be set just after deployment.
+    function enableBurnable() public virtual {
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        token().enabledBurnable = true;
+    }
+
+    /// OPTIONAL To be set just after deployment.
+    function enableAdminMarketMaker() public virtual {
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        token().enabledAdminMarketMaker = true;
+    }
+
+    /// OPTIONAL To be set just after deployment.
+    function enablePausable() public virtual {
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        token().enabledPausable = true;
+    }
+
+    /// OPTIONAL To be set just after deployment.
+    function enableTokenWrapper() public virtual {
+        require(_msgSender() == admin().admin, 'facetToken: only admin');
+        token().enabledTokenWrapper = true;
+    }
 
     function approve(address spender, uint amount) public virtual override returns (bool) {
         address owner = _msgSender();
@@ -102,13 +188,30 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
         return true;
     }
 
+    /// If settings enabled allows admin to make transfers directly on
+    /// behalf of an account.
+    function makeTransferOnBehalfOf(address from, address to, uint amount) public virtual {
+        if (token().enabledAdminMarketMaker) {
+            require(_msgSender() == admin().admin, 'facetToken: only admin');   
+            _transfer(from, to, amount);
+        } else {
+            revert('facetToken: admin market making disabled');
+        }
+    }
+
     function transfer(address to, uint amount) public virtual override returns (bool) {
+        if (token().enabledAdminMarketMaker) {
+            revert('facetToken: admin market making enabled');
+        }
         address owner_ = _msgSender();
         _transfer(owner_, to, amount);
         return true;
     }
 
     function transferFrom(address from, address to, uint amount) public virtual override returns (bool) {
+        if (token().enabledAdminMarketMaker) {
+            revert('facetToken: admin market making enabled');
+        }
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
         _transfer(from, to, amount);
@@ -116,6 +219,9 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function increaseAllowance(address spender, uint addedValue) public virtual returns (bool) {
+        if (token().enabledAdminMarketMaker) {
+            revert('facetToken: admin market making enabled');
+        }
         address owner = _msgSender();
         _approve(owner_, spender, allowance(owner_, spender) + addedValue);
         return true;
@@ -132,8 +238,20 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function _transfer(address from, address to, uint amount) internal virtual {
+        require(!pausable().paused, 'facetToken: paused');
         if (token().enabledSoulBound) {
             revert('facetToken: is soul bound');
+        }
+        if (token().enabledTransferFee) {
+            /// Sends transfer fee to destination address and sends the
+            /// remaining amount to the given 'to' address.
+            uint amountFee = (amount * token().feeTransfer) / 10000;
+            if (token().feeTransferTo == address(0)) {
+                _burn(from, amountFee);
+            } else {
+                _transfer(from, token().feeTransferTo, amountFee);
+            }
+            uint amount  = amount - amountFee;
         }
         require(from != address(0), 'facetToken: transfer from the zero address');
         require(to != address(0), 'facetToken: transfer to the zero address');
@@ -149,6 +267,7 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function _mint(address account, uint amount) internal virtual {
+        require(!pausable().paused, 'facetToken: paused');
         if (token().enabledCapped) { /// If cap is enabled enforce.
             require(totalSupply() + amount <= token().maxSupply, 'facetToken: is at cap');
         }
@@ -163,6 +282,7 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function _burn(address account, uint amount) internal virtual {
+        require(!pausable().paused, 'facetToken: paused');
         require(account != address(0), 'facetToken: burn from the zero address');
         _beforeTokenTransfer(account, address(0), amount);
         uint accountBalance = token().balances[account];
@@ -176,6 +296,7 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function _spendAllowance(address owner_, address spender, uint amount) internal virtual {
+        require(!pausable().paused, 'facetToken: paused');
         uint currentAllowance = allowance(owner_, spender);
         if (currentAllowance != type(uint).max) {
             require(currentAllowance >= amount, 'facetToken: insufficient allowance');
@@ -186,6 +307,7 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function _approve(address owner_, address spender, uint amount) internal virtual {
+        require(!pausable().paused, 'facetToken: paused');
         require(owner_ != address(0), 'facetToken: approve from the zero address');
         require(spender != address(0), 'facetToken: approve to the zero address');
         token().allowances[owner_][spender] = amount;
@@ -193,7 +315,14 @@ contract facetToken is Context, storageToken, storageAdmin, storagePausable, IER
     }
 
     function _beforeTokenTransfer(address from, address to, uint amount) internal virtual {
-
+        if (token().enabledWhitelist) { /// Allow certain accounts to transfer and receieve the token,
+            require(token().whitelist.contains(from), 'facetToken: from is not whitelisted');
+            require(token().whitelist.contains(to), 'facetToken: to is not whitelisted');
+        }
+        if (token().enabledBlacklist) { /// Forbid certain accounts to transfer and receive the token.
+            require(!token().blacklist.contains(from), 'facetToken: from is blacklisted');
+            require(!token().blacklist.contains(to), 'facetToken: to is blacklisted');
+        }
     }
 
     function _afterTokenTransfer(address from, address to, uint amount) internal virtual {
